11e2f6ce9e9aacf61c812566a0188668118ecc47
Donnie <122802443+Dnnavant@users.noreply.github.com>
2023-09-04 13:37:42 +0200

    fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 
 import Header from "./Components/Header-Comp/Header";
-import Navbar from "./Components/Navbar-Comp/Navbar";
 import Footer from "./Components/Footer-Comp/Footer";
 import RecipeSearch from "./Components/RecipeSearch/RecipeSearch";
 import AboutUs from "./Components/AboutUs/AboutUs";
-import LandingPage from "./Components/LandingPage/LandingPage";
+
 
 
 function App() {
 	return (
 		<div>
 			<Header />
-			{/* <Navbar /> */}
-			{/* <LandingPage/> */}
 			<RecipeSearch/>
 			<AboutUs />
 			<Footer />
